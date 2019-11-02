3b96b409ec0c24f4890cfcf487a242d0e250f5df
Ted Singer <tedsinger1@gmail.com>
2019-11-02 13:56:29 -0400

    qrcodes!

--- main.go
+++ main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
-	"github.com/boltdb/bolt"
-	"github.com/microcosm-cc/bluemonday"
-	"net/http"
-	"time"
+	"encoding/base64"
 	"fmt"
 	"math/rand"
-	"strings"
+	"net/http"
 	"regexp"
+	"strings"
+	"time"
+
+	"github.com/boltdb/bolt"
+	"github.com/labstack/echo/v4"
+	"github.com/microcosm-cc/bluemonday"
+	qrcode "github.com/skip2/go-qrcode"
 )
 
 const LETTERS = "abcdefghijklmnopqrstuvwxyz"
-const METABLURB = `<u><em>Blurb.cloud</em></u> is a shared, local billboard. Take an old tablet or smartphone, mount it on the wall, and browse to this page to display the blurb. Anyone with the link can change the blurb, and anyone who passes by the display can see the link.`
+const METABLURB = `<u><em style='background-color: rgb(255, 240, 201)'>Blurb.cloud</em></u> is a shared, local billboard. Take an old tablet or smartphone, mount it on the wall, and browse to this page to display the blurb. Anyone with the link can change the blurb, and anyone who passes by the display can see the link.`
 
 type BlurbServer struct {
 	db *bolt.DB
@@ -21,7 +24,7 @@ type BlurbServer struct {
 
 func main() {
 	db, _ := bolt.Open("blurbs.db", 0600, nil)
-	
+
 	cs := BlurbServer{db}
 	cs.db.Update(func(tx *bolt.Tx) error {
 		tx.CreateBucketIfNotExists([]byte("Blurbs"))
@@ -36,7 +39,7 @@ func main() {
 	e.GET("/raw/:blurb", cs.getRaw)
 	e.GET("/blurb/:blurb", cs.getBlurb)
 	// https://softwareengineering.stackexchange.com/questions/114156/why-are-there-are-no-put-and-delete-methods-on-html-forms
-	e.POST("/blurb/:blurb", cs.putBlurb) 
+	e.POST("/blurb/:blurb", cs.putBlurb)
 	e.Start(":22000")
 	defer db.Close()
 }
@@ -47,7 +50,7 @@ func getNewBlurbId() string {
 	ret := ""
 	for i := 0; i < 4; i++ {
 		n := rand.Intn(len(LETTERS))
-		ret += LETTERS[n:n+1]
+		ret += LETTERS[n : n+1]
 	}
 
 	return ret
@@ -67,7 +70,7 @@ func (cs BlurbServer) streamingUpdates(c echo.Context) error {
 			oldText = text
 			c.Response().Write([]byte("\n"))
 			c.Response().Flush()
-		} 
+		}
 		// TODO: use gochannels instead of local polling
 		time.Sleep(1 * time.Second)
 	}
@@ -106,6 +109,7 @@ func (cs BlurbServer) getEditor(c echo.Context) error {
 			var button = document.querySelector('input');
 			toolbar.insertBefore(button, toolbar.childNodes[0]);
 			var editor = document.querySelector('.ql-editor');
+			// FIXME: interacts badly with quotes in the inline styles
 			quill.clipboard.dangerouslyPasteHTML("%s", "silent");
 			textarea.style.visibility = 'hidden';
 			quill.on('text-change', function(delta) {
@@ -124,7 +128,7 @@ func (cs BlurbServer) getEditor(c echo.Context) error {
 
 func (cs BlurbServer) getRoot(c echo.Context) error {
 	c.Response().Header().Set("Cache-control", "no-cache")
-	return c.Redirect(301, "/blurb/" + getNewBlurbId())
+	return c.Redirect(301, "/blurb/"+getNewBlurbId())
 }
 
 func (cs BlurbServer) getBlurbText(blurbId string) string {
@@ -141,6 +145,15 @@ func (cs BlurbServer) getBlurbText(blurbId string) string {
 	}
 }
 
+func getPng(blurbId string) string {
+	var png []byte
+	png, err := qrcode.Encode("http://blurb.cloud/blurb/"+blurbId, qrcode.High, 120)
+	if err != nil {
+		return ""
+	} else {
+		return base64.StdEncoding.EncodeToString(png)
+	}
+}
 
 func (cs BlurbServer) getRaw(c echo.Context) error {
 	blurbId := c.Param("blurb")
@@ -155,7 +168,6 @@ func (cs BlurbServer) getBlurb(c echo.Context) error {
 	<head>
 		<script type="text/javascript" src="/static/viewfuncs.js"></script>
 		<style>
-			button { margin: auto; display: block; }
 			p { margin: 0em; }
 			#text { letter-spacing: 1px; line-height: 1.4; font-family: Sans-serif; word-wrap: break-word; height: auto;}
 		</style>
@@ -167,7 +179,10 @@ func (cs BlurbServer) getBlurb(c echo.Context) error {
 				<div id="text">
 					%s
 				</div>
-				<a href="%s"><button>Edit: %s</button></a>
+				<div style="display: flex; justify-content: center;align-items: center;">
+					<img src="data:image/png;base64, %s" alt="qrcode link to this page"></img>
+					<a href="%s"><button>Edit<br>me!</button></a>
+				</div>
 			</div>
 		</body>
 		<script type="text/javascript">
@@ -178,13 +193,13 @@ func (cs BlurbServer) getBlurb(c echo.Context) error {
 		</script>
 	</html>
 	`
-	return c.HTML(http.StatusOK, 
-		fmt.Sprintf(template, 
+	return c.HTML(http.StatusOK,
+		fmt.Sprintf(template,
 			strings.ReplaceAll(string(text), "\n", "<br>"),
-			"/editor/" + blurbId,
-			"/blurb/" + blurbId,
-			"/raw/" + blurbId,
-			"/stream/" + blurbId))
+			getPng(blurbId),
+			"/editor/"+blurbId,
+			"/raw/"+blurbId,
+			"/stream/"+blurbId))
 }
 
 func rgbOfInts(s string) bool {
@@ -207,7 +222,7 @@ func (cs BlurbServer) putBlurb(c echo.Context) error {
 		return err
 	})
 	if err == nil {
-		return c.Redirect(http.StatusSeeOther, "/blurb/" + blurbId)
+		return c.Redirect(http.StatusSeeOther, "/blurb/"+blurbId)
 	} else {
 		return c.String(400, err.Error())
 	}
