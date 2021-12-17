f040b8e2741c39b7806855a5f98d6760a3cf1da1
Ted Singer <tedsinger1@gmail.com>
2021-12-17 17:12:36 -0500

    use mithril

--- static/viewFuncs.js
+++ static/viewFuncs.js
@@ -1,45 +1,55 @@
-function fit() {
-    var textDiv = document.querySelector("#text");
-    var body = document.querySelector("#all")
-    textDiv.style.visibility = "hidden";
-    var tooSmall = 0;
-    var tooBig = 128;
-    while (tooBig > tooSmall + 1) {
-        middle = (tooSmall + tooBig) / 2;
-        textDiv.style.fontSize = middle;
-        if (window.innerHeight > body.clientHeight) {
-            tooSmall = middle;
-        } else {
-            tooBig = middle;
-        }
-    }
-    textDiv.style.fontSize = tooSmall;
-    textDiv.style.visibility = "visible";
-};
-
-function useText(t) {
-    document.getElementById("text").innerHTML = t;
-    fit();
-};
-
-function loadBlurb(url) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            useText(this.responseText);
-        }
+class BlurbHolder {
+    constructor() {
+        this.text = document.querySelector("#text").innerHTML;
     };
-    xhttp.open("GET", url, true);
-    xhttp.send();
+    view() {
+        return m("div", {}, m.trust(this.text));
+    };
+    onupdate() {
+        var textDiv = document.querySelector("#text");
+        var body = document.querySelector("#all")
+        textDiv.style.visibility = "hidden";
+        var tooSmall = 0;
+        var tooBig = 128;
+        while (tooBig > tooSmall + 1) {
+            var middle = (tooSmall + tooBig) / 2;
+            textDiv.style.fontSize = middle;
+            if (window.innerHeight > body.clientHeight) {
+                tooSmall = middle;
+            } else {
+                tooBig = middle;
+            }
+        }
+        textDiv.style.fontSize = tooSmall;
+        textDiv.style.visibility = "visible";
+    };
+    oncreate = this.onupdate;
+
+    useText(t) {
+        var self = this;
+        self.text = t;
+        m.redraw();
+    };
+
+}
+
+function getRawText(url) {
+    return m.request(url, {
+        background: true,
+        deserialize: function(value) {return value}, 
+        responseType: "", 
+        extract: function(xhr) {return xhr.responseText}
+    })
 };
 
-function watchUpdates(rawurl, streamurl) {
-    if (typeof (EventSource) !== "undefined") {
+function watchUpdates(rawurl, streamurl, method) {
+    if (typeof(EventSource) !== "undefined") {
         var source = new EventSource(streamurl);
-        source.onmessage = function (event) { useText(event.data); }
-        source.onerror = function () { setTimeout(function () { watchUpdates(rawurl, streamurl); }, 10 * 60 * 1000); }
+        source.onmessage = function (event) { method(event.data); }
+        source.onerror = function () { setTimeout(function () { watchUpdates(rawurl, streamurl, method); }, 10 * 60 * 1000); }
     } else {
-        setTimeout(function () { loadBlurb(rawurl); }, 10 * 60 * 1000);
-        setTimeout(function () { watchUpdates(rawurl, streamurl); }, 10 * 60 * 1000);
+        // Long-poll?
+        setTimeout(function () { getRawText(rawurl).then((x) => {method(x)}); }, 1 * 1000);
+        setTimeout(function () { watchUpdates(rawurl, streamurl, method); }, 1 * 1000);
     }
 };
